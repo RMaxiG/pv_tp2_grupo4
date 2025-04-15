0641d183cc3211d5c8046cdae9635e53ea51be4b
MarcosM09 <marcos.antonio.mendez2002@gmail.com>
2025-04-14 21:56:58 -0300

    Ejercicios 3,6,9 resueltos.

---
+++ js/ejercicio3.js
@@ -0,0 +1,5 @@
+let pi = 3.1416;
+let radio = 7.5;
+
+let area = pi * (radio ** 2 );
+console.log("El area del circulo es: " + area);
---
+++ js/ejercicio6.js
@@ -0,0 +1,6 @@
+var edades = [4,6,9,12,16,23,25,29];
+var i=0;
+while (i<edades.length){
+    console.log("Edad: "+ edades[i]);
+    i++;
+}

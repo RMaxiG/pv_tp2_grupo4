8d81b01681221a5e68621093a6dd560e312a4416
Mili Martinez <milimartinez.uno@gmail.com>
2025-04-17 18:43:32 -0300

    ejercicio 21

---
+++ js/ejercicio21.js
@@ -0,0 +1,4 @@
+export const historiaEscrita = () => {
+const historia = document.getElementById('historia');
+const textoIngresado = document.getElementById('textoIngresado');
+textoIngresado.textContent = historia.value;}
